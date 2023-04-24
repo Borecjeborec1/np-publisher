24db72a2ea1d0f08732a4fcb2f0f7975c345305d
borecjeborec1 <robingrundel@seznam.cz>
2023-04-24 12:56:59 +0200

    args handling

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const { execSync } = require('child_process');
 const fs = require("fs")
 
-
-
-
 exports.publish = async function publish(args) {
   if (args == "-v") return console.log(require("./package.json").version);
+  let incrIndex = args == "-major" ? 0 : args == "-minor" ? 1 : 2
   let jsonPath = __dirname + "/package.json"
   let pjson = require(jsonPath)
   let numbers = pjson.version.split(".").map(Number)
-  numbers[2] += 1
+  let oldVersion = pjson.version.split(".").map(Number).join(".")
+  numbers[incrIndex] += 1
   pjson.version = numbers.join(".")
-  console.log(pjson)
+  console.log("Publishing to npm.")
+  console.log(`Version changed ${oldVersion} -> ${pjson.version}.`)
   fs.writeFileSync(jsonPath, JSON.stringify(pjson))
   execSync("npm publish")
 }
