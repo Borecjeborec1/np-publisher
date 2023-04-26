0c96ac52f92b53d574a22061dcbc749b6d0b685a
borecjeborec1 <robingrundel@seznam.cz>
2023-04-26 08:29:34 +0200

    sync way

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const { execSync } = require('child_process');
-const fs = require("fs")
+const { spawn, spawnSync, execSync } = require('child_process');
+const fs = require("fs");
+const which = require('which');
 
 exports.publish = async function publish(args) {
   if (args == "-v") return console.log(require("./package.json").version);
@@ -12,8 +13,19 @@ exports.publish = async function publish(args) {
   pjson.version = numbers.join(".")
   console.log("Publishing to npm.")
   console.log(`Version changed ${oldVersion} -> ${pjson.version}.`)
-  fs.writeFileSync(jsonPath, JSON.stringify(pjson))
-  execSync("npm publish")
+  fs.writeFileSync(jsonPath, JSON.stringify(pjson));
+  const npmPath = which.sync('npm');
+  const publishProcess = spawn(`${npmPath}`, ['publish']);
+
+  publishProcess.stdout.on('data', (data) => {
+    console.log(data.toString());
+  });
+
+  publishProcess.stderr.on('data', (data) => {
+    console.error(data.toString());
+  });
+
+  publishProcess.on('error', (err) => {
+    console.error(err);
+  });
 }
-
-
